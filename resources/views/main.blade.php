4f2e2cd9b3e770eac554045927fc377bfeb359b1
Adriana Contreras <adriana.contreras@futurite.com>
2023-09-06 16:24:56 -0600

    token

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') | Compumedic</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
     @yield('styles')
